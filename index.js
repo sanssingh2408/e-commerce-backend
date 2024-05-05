504c142dc78473b36a21c5adb3089b1b89109710
Sanskar Singh <sanskarsingh@Sanskars-MacBook-Air.local>
2024-05-05 23:16:35 +0530

    first commit

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({ extended: true }))
 
 
-
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 app.use(cors({
     origin: 'https://e-commerce-psi-three-88.vercel.app',
